MrFox42/minarch

commit 747d1b9f720f9414e4efdc632956fb75ce60c6a9

Fix history refresh

addon/components/History.lua
@@ -227,7 +227,7 @@ end
 local function BuildHistory(RaceID, caller)
     MinArch:DisplayStatusMessage("BuildHistory " .. caller, MINARCH_MSG_DEBUG)
 
-    local i = unknownArtifactInfoIndex[RaceID];
+    local i = 1 -- unknownArtifactInfoIndex[RaceID];
     MinArch:DisplayStatusMessage("Bulding history for race " .. RaceID .. " from index: " .. i, MINARCH_MSG_DEBUG)
 	while true do
 		local name, desc, rarity, icon, spelldesc, itemrare, _, spellId, firstcomplete, totalcomplete = GetArtifactInfoByRace(RaceID, i)
@@ -243,7 +243,7 @@ local function BuildHistory(RaceID, caller)
 		end
 		icon = icon..".blp"
 
-		local foundCount = 0
+		-- local foundCount = 0
         for itemid, details in pairs(MinArchHistDB[RaceID]) do
 			if (details.name == name and details.icon ~= icon) then
 				MinArchIconDB[RaceID] = MinArchIconDB[RaceID] or {}
@@ -259,15 +259,15 @@ local function BuildHistory(RaceID, caller)
 
         for itemid, details in pairs(MinArchHistDB[RaceID]) do
             if ((details.name == name and details.icon == icon) or (foundCount == 0 and details.icon == icon)) then
-                foundCount = foundCount + 1
-                if foundCount > 1 then
-                    MinArch:DisplayStatusMessage("Minimal Archaeology - found duplicate #" .. foundCount, MINARCH_MSG_DEBUG)
-                    MinArch:DisplayStatusMessage("Race " .. RaceID .. ": " .. (MinArch.artifacts[RaceID].race or ("Race" .. RaceID)), MINARCH_MSG_DEBUG)
-                    MinArch:DisplayStatusMessage("Item " .. itemid .. ": " .. details.name, MINARCH_MSG_DEBUG)
-                    MinArch:DisplayStatusMessage("Artifact: " .. name, MINARCH_MSG_DEBUG)
-                    MinArch:DisplayStatusMessage("Item icon '" .. details.icon .. "'", MINARCH_MSG_DEBUG)
-                    MinArch:DisplayStatusMessage("Artifact icon '" .. icon .. "'", MINARCH_MSG_DEBUG)
-                end
+                -- foundCount = foundCount + 1
+                -- if foundCount > 1 then
+                --     MinArch:DisplayStatusMessage("Minimal Archaeology - found duplicate #" .. foundCount, MINARCH_MSG_DEBUG)
+                --     MinArch:DisplayStatusMessage("Race " .. RaceID .. ": " .. (MinArch.artifacts[RaceID].race or ("Race" .. RaceID)), MINARCH_MSG_DEBUG)
+                --     MinArch:DisplayStatusMessage("Item " .. itemid .. ": " .. details.name, MINARCH_MSG_DEBUG)
+                --     MinArch:DisplayStatusMessage("Artifact: " .. name, MINARCH_MSG_DEBUG)
+                --     MinArch:DisplayStatusMessage("Item icon '" .. details.icon .. "'", MINARCH_MSG_DEBUG)
+                --     MinArch:DisplayStatusMessage("Artifact icon '" .. icon .. "'", MINARCH_MSG_DEBUG)
+                -- end
 
                 --TODO: In the tooltip, display icon/name/info for artifact and all associated item icons
                 -- Change MinArchHistDB to include the alternate item IDs (for example, Orb of Sciallax can give 6 different relics items)
@@ -315,12 +315,12 @@ function MinArch:GetHistory(RaceID, caller)
         else
             local previousCompleted = details.totalcomplete;
             local name, desc, _, _, spelldesc, _, _, _, firstcomplete, totalcomplete = GetArtifactInfoByRace(RaceID, details.apiIndex)
-            if (previousCompleted and previousCompleted > 0 and previousCompleted > totalcomplete) then
-                -- Don't update stored data if the response is bogus
-                MinArch:DisplayStatusMessage("Bogus data from API, refreshing ...", MINARCH_MSG_DEBUG)
-                MinArch:DelayedHistoryUpdate();
-                return;
-            end
+            -- if (previousCompleted and previousCompleted > 0 and previousCompleted > totalcomplete) then
+            --     -- Don't update stored data if the response is bogus
+            --     MinArch:DisplayStatusMessage("Bogus data from API, refreshing ...", MINARCH_MSG_DEBUG)
+            --     MinArch:DelayedHistoryUpdate();
+            --     return;
+            -- end
 
             details.artifactname = name
             details.firstcomplete = firstcomplete

commit ab55ba17115daeb2b1b078d3df8e88b663979f4e

Fix history issue caused by commit 747d1b9, bump Mainline toc, stage 10.2.10

MinimalArchaeology-Cata.toc
@@ -2,7 +2,7 @@
 ## Title: Minimal Archaeology
 ## Author: Gyula Pataki, Joseph O'Connor, Josef Drexler
 ## Email: gyussz@live.com
-## Version: 10.2.9
+## Version: 10.2.10
 ## Notes: Archaeology UI replacement, Archaeology History and more... - Cataclysm Version
 ## DefaultState: Enabled
 ## LoadOnDemand: 0

MinimalArchaeology-Mainline.toc
@@ -1,8 +1,8 @@
-## Interface: 100206
+## Interface: 100207
 ## Title: Minimal Archaeology
 ## Author: Gyula Pataki, Joseph O'Connor, Josef Drexler
 ## Email: gyussz@live.com
-## Version: 10.2.9
+## Version: 10.2.10
 ## Notes: Archaeology UI replacement, Archaeology History and more... - Mainline version
 ## DefaultState: Enabled
 ## LoadOnDemand: 0

addon/components/History.lua
@@ -269,7 +269,7 @@ local function BuildHistory(RaceID, caller)
 		end
 		icon = icon..".blp"
 
-		-- local foundCount = 0
+		local foundCount = 0
         for itemid, details in pairs(MinArchHistDB[RaceID]) do
 			if (details.name == name and details.icon ~= icon) then
 				MinArchIconDB[RaceID] = MinArchIconDB[RaceID] or {}
@@ -285,7 +285,7 @@ local function BuildHistory(RaceID, caller)
 
         for itemid, details in pairs(MinArchHistDB[RaceID]) do
             if ((details.name == name and details.icon == icon) or (foundCount == 0 and details.icon == icon)) then
-                -- foundCount = foundCount + 1
+                foundCount = foundCount + 1
                 -- if foundCount > 1 then
                 --     MinArch:DisplayStatusMessage("Minimal Archaeology - found duplicate #" .. foundCount, MINARCH_MSG_DEBUG)
                 --     MinArch:DisplayStatusMessage("Race " .. RaceID .. ": " .. (MinArch.artifacts[RaceID].race or ("Race" .. RaceID)), MINARCH_MSG_DEBUG)
@@ -341,12 +341,13 @@ function MinArch:GetHistory(RaceID, caller)
         else
             local previousCompleted = details.totalcomplete;
             local name, desc, _, _, spelldesc, _, _, _, firstcomplete, totalcomplete = GetArtifactInfoByRace(RaceID, details.apiIndex)
-            -- if (previousCompleted and previousCompleted > 0 and previousCompleted > totalcomplete) then
-            --     -- Don't update stored data if the response is bogus
-            --     MinArch:DisplayStatusMessage("Bogus data from API, refreshing ...", MINARCH_MSG_DEBUG)
-            --     MinArch:DelayedHistoryUpdate();
-            --     return;
-            -- end
+            if (previousCompleted and previousCompleted > 0 and previousCompleted > totalcomplete) then
+                -- Don't update stored data if the response is bogus
+                MinArch:DisplayStatusMessage("Bogus data from API, skipping refresh", MINARCH_MSG_DEBUG)
+                --BuildHistory(RaceID, 'GetHistory');
+                --MinArch:DelayedHistoryUpdate();
+                return;
+            end
 
             details.artifactname = name
             details.firstcomplete = firstcomplete